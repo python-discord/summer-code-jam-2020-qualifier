a12ff552357f9dff1235138d3b209aacc3d03d94
Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>
2020-05-26 11:55:46 +0200

    Add the base solution file participants should use
    I've added the solutions file, `solution.py`, that the participants
    should use when they write their solution. It includes instructions for
    the submission of the solution and the two stub classes that should be
    expanded upon.

---
+++ solution.py
@@ -0,0 +1,26 @@
+"""
+Use this file to write your solution for the Summer Code Jam 2020 Qualifier.
+
+Important notes for submission:
+
+- Do not change the names of the two classes included below. The test suite we
+  will use to test your submission relies on existence these two classes.
+
+- You can leave the `ArticleField` class as-is if you do not wish to tackle the
+  advanced requirements.
+
+- Do not include "debug"-code in your submission. This means that you should
+  remove all debug prints and other debug statements before you submit your
+  solution.
+"""
+
+
+class ArticleField:
+    """
+    The `ArticleField` class for the Advanced Requirements. Leave this class
+    as-is if you do not want to tackle the advanced requirements.
+    """
+
+
+class Article:
+    """The `Article` class you need to write for the qualifier."""
