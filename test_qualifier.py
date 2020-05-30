835ea449fba501996591ab0da278b0d24a183366
MarkKoz <KozlovMark@gmail.com>
2020-05-30 13:31:30 -0700

    Test basic requirement 1

--- test_qualifier.py
+++ test_qualifier.py
@@ -1,1 +1,29 @@
-"""Stub file for the test suite."""
+import datetime
+import unittest
+
+from qualifier import Article
+
+
+class BasicTests(unittest.TestCase):
+    """Tests for the basic requirements."""
+
+    def setUp(self) -> None:
+        """Create a new Article instance before running each test."""
+        self.title = "The emperor's new clothes"
+        self.author = "Hans Christian Andersen"
+        self.content = "'But he has nothing at all on!' at last cried out all the people."
+        self.publication_date = datetime.datetime(1837, 4, 7, 12, 15, 0)
+
+        self.article = Article(
+            title=self.title,
+            author=self.author,
+            content=self.content,
+            publication_date=self.publication_date
+        )
+
+    def test_instantiation(self):
+        """The parameters given to __init__ should be assigned to attributes with the same names."""
+        self.assertEqual(self.title, self.article.title)
+        self.assertEqual(self.author, self.article.author)
+        self.assertEqual(self.content, self.article.content)
+        self.assertEqual(self.publication_date, self.article.publication_date)
